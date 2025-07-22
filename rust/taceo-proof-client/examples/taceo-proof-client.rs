8484f851fe5ce03092e9364855d144ee449153ce
Fabian Gruber <fabian.gruber5@gmail.com>
2025-07-22 15:40:32 +0200

    feat: split rust cli example args for prove and full-prove

--- rust/taceo-proof-client/examples/taceo-proof-client.rs
+++ rust/taceo-proof-client/examples/taceo-proof-client.rs
@@ -3,7 +3,7 @@ use std::{fs::File, path::PathBuf, time::Instant};
 use ark_bls12_377::Bls12_377;
 use ark_bls12_381::Bls12_381;
 use ark_bn254::Bn254;
-use clap::{ArgGroup, Parser, ValueEnum};
+use clap::{ArgGroup, Parser, Subcommand, ValueEnum};
 use taceo_proof_client::{
     SignedResults, StopStrategy,
     apis::configuration::Configuration,
@@ -29,27 +29,25 @@ enum Curve {
     Bls377,
 }
 
-#[derive(Parser, Debug)]
-#[clap(group(
-    ArgGroup::new("exclusive_args")
-        .args(&["r1cs", "num_inputs"])
-        .required(true)
-))]
-struct Args {
-    /// The job type
-    pub job: JobType,
+#[derive(Debug, Clone, Copy, ValueEnum)]
+enum MpcProtocol {
+    REP3,
+    Shamir,
+}
 
+#[derive(Parser, Debug, Clone)]
+struct FullProve {
     /// The API endpoint URL
-    #[clap(long, env = "PROOF_API_URL", default_value = "http://localhost:1234")]
+    #[clap(
+        long,
+        env = "TACEO_PROOF_API_URL",
+        default_value = "https://proof.taceo.network"
+    )]
     pub api_url: String,
 
     /// The curve
     #[clap(long, env = "PROOF_CURVE")]
     pub curve: Curve,
-
-    /// The path to the job input
-    #[clap(long, env = "PROOF_INPUT")]
-    pub input: PathBuf,
 
     /// The voucher for a proof job
     #[clap(long, env = "PROOF_VOUCHER")]
@@ -59,16 +57,12 @@ struct Args {
     #[clap(long, env = "PROOF_BLUEPRINT")]
     pub blueprint: Uuid,
 
-    /// The path to the r1cs file
-    #[clap(long, env = "PROOF_R1CS")]
-    pub r1cs: Option<PathBuf>,
-
-    /// The number of inputs for the circuit
-    #[clap(long, env = "PROOF_NUM_INPUTS")]
-    pub num_inputs: Option<usize>,
+    /// The path to the job input
+    #[clap(long, env = "PROOF_INPUT")]
+    pub input: PathBuf,
 
     /// The public inputs for witness extension
-    #[clap(long, env = "PROOF_PUBLIC_INPUTS", required_if_eq("job", "rep3-full"))]
+    #[clap(long, env = "PROOF_PUBLIC_INPUTS")]
     pub public_inputs: Option<Vec<String>>,
 
     /// The output file where the final proof is written to
@@ -80,24 +74,113 @@ struct Args {
     pub out_public_inputs: PathBuf,
 }
 
+#[derive(Parser, Debug, Clone)]
+#[clap(group(
+    ArgGroup::new("exclusive_args")
+        .args(&["r1cs", "num_inputs"])
+        .required(true)
+))]
+struct Prove {
+    /// The API endpoint URL
+    #[clap(
+        long,
+        env = "TACEO_PROOF_API_URL",
+        default_value = "https://proof.taceo.network"
+    )]
+    pub api_url: String,
+
+    /// The curve
+    #[clap(long, env = "PROOF_CURVE")]
+    pub curve: Curve,
+
+    /// The voucher for a proof job
+    #[clap(long, env = "PROOF_VOUCHER")]
+    pub voucher: Option<String>,
+
+    /// The job blueprint
+    #[clap(long, env = "PROOF_BLUEPRINT")]
+    pub blueprint: Uuid,
+
+    /// The MPC protocol
+    #[clap(long, env = "PROOF_MPC_PROTOCOL", default_value = "rep3")]
+    pub protocol: MpcProtocol,
+
+    /// The path to the witness file
+    #[clap(long, env = "PROOF_WITNESS")]
+    pub witness: PathBuf,
+
+    /// The path to the r1cs file
+    #[clap(long, env = "PROOF_R1CS")]
+    pub r1cs: Option<PathBuf>,
+
+    /// The number of inputs for the circuit
+    #[clap(long, env = "PROOF_NUM_INPUTS")]
+    pub num_inputs: Option<usize>,
+
+    /// The output file where the final proof is written to
+    #[arg(long, env = "PROOF_OUT", default_value = "proof.json")]
+    pub out: PathBuf,
+
+    /// The output JSON file where the public inputs are written to
+    #[arg(long, env = "PROOF_OUT_PUBLIC_INPUTS", default_value = "public.json")]
+    pub out_public_inputs: PathBuf,
+}
+
+#[derive(Debug, Clone, Subcommand)]
+enum Commands {
+    /// Schedule a full coSNARK job including witness extension
+    FullProve(FullProve),
+    /// Schedule a prove coSNARK job
+    Prove(Prove),
+}
+
+impl Commands {
+    pub fn curve(&self) -> Curve {
+        match self {
+            Commands::FullProve(full_prove) => full_prove.curve,
+            Commands::Prove(prove) => prove.curve,
+        }
+    }
+
+    pub fn api_url(&self) -> String {
+        match self {
+            Commands::FullProve(full_prove) => full_prove.api_url.clone(),
+            Commands::Prove(prove) => prove.api_url.clone(),
+        }
+    }
+
+    pub fn out(&self) -> PathBuf {
+        match self {
+            Commands::FullProve(full_prove) => full_prove.out.clone(),
+            Commands::Prove(prove) => prove.out.clone(),
+        }
+    }
+
+    pub fn out_public_inputs(&self) -> PathBuf {
+        match self {
+            Commands::FullProve(full_prove) => full_prove.out_public_inputs.clone(),
+            Commands::Prove(prove) => prove.out_public_inputs.clone(),
+        }
+    }
+}
+
+#[derive(Parser, Debug, Clone)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
 async fn run<P>(config: &Configuration, args: Args) -> eyre::Result<()>
 where
     P: Pairing + CircomArkworksPairingBridge,
     P::ScalarField: CircomArkworksPrimeFieldBridge,
     P::BaseField: CircomArkworksPrimeFieldBridge,
 {
-    let num_inputs = if let Some(r1cs) = args.r1cs {
-        let r1cs = R1CS::<P>::from_reader(File::open(r1cs)?)?;
-        r1cs.num_inputs
-    } else {
-        args.num_inputs.expect("must be present if r1cs is not")
-    };
-
     let nodes = taceo_proof_client::get_random_node_providers(config).await?;
 
     let start = Instant::now();
-    let job_id = match args.job {
-        JobType::Rep3Full => {
+    let job_id = match args.command.clone() {
+        Commands::FullProve(args) => {
             let input = serde_json::from_reader(File::open(args.input)?)?;
             taceo_proof_client::schedule_full_job_rep3::<P>(
                 config,
@@ -111,35 +194,48 @@ where
             )
             .await?
         }
-        JobType::Rep3Prove => {
-            let witness = Witness::from_reader(File::open(args.input)?)?;
-            taceo_proof_client::schedule_prove_job_rep3::<P>(
-                config,
-                &nodes,
-                args.blueprint,
-                args.voucher.as_deref(),
-                num_inputs,
-                witness,
-            )
-            .await?
-        }
-        JobType::ShamirProve => {
-            let witness = Witness::from_reader(File::open(args.input)?)?;
-            taceo_proof_client::schedule_prove_job_shamir::<P>(
-                config,
-                &nodes,
-                args.blueprint,
-                args.voucher.as_deref(),
-                num_inputs,
-                witness,
-            )
-            .await?
+        Commands::Prove(args) => {
+            let num_inputs = if let Some(r1cs) = args.r1cs {
+                let r1cs = R1CS::<P>::from_reader(File::open(r1cs)?)?;
+                r1cs.num_inputs
+            } else {
+                args.num_inputs.expect("must be present if r1cs is not")
+            };
+            let witness = Witness::from_reader(File::open(args.witness)?)?;
+            match args.protocol {
+                MpcProtocol::REP3 => {
+                    taceo_proof_client::schedule_prove_job_rep3::<P>(
+                        config,
+                        &nodes,
+                        args.blueprint,
+                        args.voucher.as_deref(),
+                        num_inputs,
+                        witness,
+                    )
+                    .await?
+                }
+                MpcProtocol::Shamir => {
+                    taceo_proof_client::schedule_prove_job_shamir::<P>(
+                        config,
+                        &nodes,
+                        args.blueprint,
+                        args.voucher.as_deref(),
+                        num_inputs,
+                        witness,
+                    )
+                    .await?
+                }
+            }
         }
     };
     tracing::info!("scheduled job {job_id}");
 
-    let ws_url =
-        args.api_url.replace("http", "ws").replace("https", "wss") + "/api/v1/reports/subs";
+    let ws_url = args
+        .command
+        .api_url()
+        .replace("http", "ws")
+        .replace("https", "wss")
+        + "/api/v1/reports/subs";
     // contains the proof and public_inputs as JSON strings for a CircomGroth16 proof
     // or as base64 encoded ark_serialize serialized bytes for a LibsnarkGroth16 proof
     let SignedResults {
@@ -149,14 +245,14 @@ where
     } = taceo_proof_client::fetch_job_result(&ws_url, job_id, StopStrategy::default()).await?;
     tracing::info!("job took {}s", start.elapsed().as_secs_f64());
 
-    std::fs::write(&args.out, proof)?;
-    tracing::info!("wrote proof to {}", args.out.display());
+    let out = args.command.out();
+    let out_public_inputs = args.command.out_public_inputs();
 
-    std::fs::write(&args.out_public_inputs, public_inputs)?;
-    tracing::info!(
-        "wrote public inputs to {}",
-        args.out_public_inputs.display()
-    );
+    std::fs::write(&out, proof)?;
+    tracing::info!("wrote proof to {}", out.display());
+
+    std::fs::write(&out_public_inputs, public_inputs)?;
+    tracing::info!("wrote public inputs to {}", out_public_inputs.display());
 
     Ok(())
 }
@@ -185,11 +281,11 @@ async fn main() -> eyre::Result<()> {
 
     let args = Args::parse();
     let config = Configuration {
-        base_path: args.api_url.clone(),
+        base_path: args.command.api_url(),
         ..Default::default()
     };
 
-    match args.curve {
+    match args.command.curve() {
         Curve::Bn254 => run::<Bn254>(&config, args).await?,
         Curve::Bls381 => run::<Bls12_381>(&config, args).await?,
         Curve::Bls377 => run::<Bls12_377>(&config, args).await?,
